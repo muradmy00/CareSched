2df1bf489a5f892c6d8fc8ec9365ce349bb595a6
Mohiuddin_Murad <104640147+muradmy00@users.noreply.github.com>
2024-10-05 00:15:59 +0600

    Update TopDoctors.jsx

--- src/Components/TopDoctors.jsx
+++ src/Components/TopDoctors.jsx
@@ -12,7 +12,7 @@ const TopDoctors = () => {
 
     <div className="flex flex-col items-center gap-4 m-16 text-gray-900 md:mx-10">
 
-     <h1 className="text-3xl font-medium">Top Doctors List</h1>
+     <h1 className="text-2xl font-medium">Top Doctors List</h1>
           <p className="sm:w-1/3 text-center text-sm">Simply browse through our extensive list of trusted doctors.</p>
       
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
